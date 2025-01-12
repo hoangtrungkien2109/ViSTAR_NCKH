16eb9e519e83dbf8f830102fa04ef9f2e1b1dd01
hoangtrungkien2109 <hoangtrungkien2109@gmail.com>
2025-01-12 14:34:52 +0700

    Add: Streaming Services (queues)

--- src/streaming/main.py
+++ src/streaming/main.py
@@ -4,20 +4,19 @@ from concurrent import futures
 import grpc
 from grpc_interceptor import ExceptionToStatusInterceptor
 
-from pb.streaming_pb2_grpc import add_StreamingServicer_to_server
-from services.frame_service import BakeryBaseService
+from src.streaming.pb.streaming_pb2_grpc import add_StreamingServicer_to_server
+from src.streaming.services.streaming_service import StreamingBaseService
 
 
-class BakeryService(BakeryBaseService):
+class StreamingService(StreamingBaseService):
     pass
-
 
 def serve():
     interceptors = [ExceptionToStatusInterceptor()]
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
     )
-    add_StreamingServicer_to_server(BakeryService(), server)
+    add_StreamingServicer_to_server(StreamingService(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
--- src/streaming/services/streaming_service.py
+++ src/streaming/services/streaming_service.py
@@ -1,31 +1,67 @@
+import threading
+from queue import Queue
+
 from grpc import StatusCode
-from grpc_interceptor.exceptions import NotFound, GrpcException
+from grpc_interceptor.exceptions import GrpcException
 
-from pb.streaming_pb2 import OrderResponse
-from pb.streaming_pb2_grpc import BakeryServicer
-
-mock_desserts = {
-    "cookie": 10,
-    "donut": 5,
-    "gelato": 0
-}
+import src.streaming.pb.streaming_pb2 as streaming_pb2
+import src.streaming.pb.streaming_pb2_grpc as streaming_pb2_grpc
 
 
-class BakeryBaseService(BakeryServicer):
+class StreamingBaseService(streaming_pb2_grpc.StreamingServicer):
+    _instance = None
+    _lock = threading.Lock()
 
-    def GetOrder(self, request, context):
-        desserts_stock = mock_desserts.get(request.order)
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super(StreamingBaseService, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
-        if desserts_stock is None:
-            raise GrpcException(
-                details="Dessert not Found",
-                status_code=StatusCode.NOT_FOUND,
-            )
+    def __init__(self):
+        if not hasattr(self, "initialized"):
+            self.initialized = True
 
-        if desserts_stock == 0:
-            raise NotFound(
-                details="Dessert out of stock",
-                status_code=StatusCode.NOT_FOUND,
-            )
+            self.text_queue = Queue()
+            self.frame_queue = Queue()
+            self.image_queue = Queue()
 
-        return OrderResponse(order_status="Delivery!")
+    def PushText(self, request, context):
+        try:
+            self.text_queue.put(request.text)
+            return streaming_pb2.PushTextResponse(request_status="Success")
+        except Exception as e:
+            raise GrpcException(status_code=StatusCode.INTERNAL, details=str(e)) from e
+
+    def PopText(self, request, context):
+        if self.text_queue.empty():
+            raise GrpcException(status_code=StatusCode.NOT_FOUND, details="No text available in the queue.")
+        text = self.text_queue.get()
+        return streaming_pb2.PopTextResponse(request_status="Success", text=text)
+
+    def PushFrame(self, request, context):
+        try:
+            self.frame_queue.put(request.frame)
+            return streaming_pb2.PushFrameResponse(request_status="Success")
+        except Exception as e:
+            raise GrpcException(status_code=StatusCode.INTERNAL, details=str(e)) from e
+
+    def PopFrame(self, request, context):
+        if self.frame_queue.empty():
+            raise GrpcException(status_code=StatusCode.NOT_FOUND, details="No frame available in the queue.")
+        frame = self.frame_queue.get()
+        return streaming_pb2.PopFrameResponse(request_status="Success", frame=frame)
+
+    def PushImage(self, request, context):
+        try:
+            self.image_queue.put(request.image_base64)
+            return streaming_pb2.PushImageResponse(request_status="Success")
+        except Exception as e:
+            raise GrpcException(status_code=StatusCode.INTERNAL, details=str(e)) from e
+
+    def PopImage(self, request, context):
+        if self.image_queue.empty():
+            raise GrpcException(status_code=StatusCode.NOT_FOUND, details="No image available in the queue.")
+        image = self.image_queue.get()
+        return streaming_pb2.PopImageResponse(request_status="Success", text=image)
