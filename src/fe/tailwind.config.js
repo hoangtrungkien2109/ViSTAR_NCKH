c1f8a5f0c99dcfd976e0e1b7f287e9f3a3c09ed2
tungooxx <tungooxx@gmail.com>
2025-02-08 21:08:04 +0700

    Commit FE

---
+++ src/fe/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/fe/templates/**/*.html",
+    "./src/fe/static/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
